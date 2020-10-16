bab6b23804fd45ee3f3807b8c31f94f11945f70e
Nicole Reed <nickreed033@gmail.com>
2020-10-16 16:57:22 +0300

    Updates

---
+++ precache-manifest.c0fc67b3bc9c83e24ce7f76ffdeaf701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd44d47099e5a6e966a39043c465effc",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "f836d7da7be91df5a934",
+    "url": "/markdown-previewer/static/css/main.bd8170a4.chunk.css"
+  },
+  {
+    "revision": "b86930c6842a9fc09620",
+    "url": "/markdown-previewer/static/js/2.ce4864bf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/markdown-previewer/static/js/2.ce4864bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f836d7da7be91df5a934",
+    "url": "/markdown-previewer/static/js/main.10d120b2.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
